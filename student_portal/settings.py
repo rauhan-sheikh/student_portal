1bbd6778ea40ce39c025f4e3624e38ee30dd5183
rauhan-sheikh <rauhan1998@gmail.com>
2025-08-26 07:52:38 +0000

    added environment variables & requirements.txt

--- student_portal/settings.py
+++ student_portal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fja6#iajx!*tlk2=$2v20qrk)8h90pjd@$+q_4+h5f0)3x@$od'
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
